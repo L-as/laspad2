1056171291d65c7a791f909cbe6511f88c1118f5
Las <las@protonmail.ch>
2018-02-14 22:26:44 +0100

    Fix previous commit

--- src/md_to_bb.rs
+++ src/md_to_bb.rs
@@ -6,23 +6,23 @@ lazy_static! {
 	static ref CODE3:  Regex = Regex::new(r#"```(.*?)```"#       ).unwrap();
 	static ref CODE2:  Regex = Regex::new(r#"``(.*?)``"#         ).unwrap();
 	static ref CODE1:  Regex = Regex::new(r#"`(.*?)`"#           ).unwrap();
-	static ref H2:     Regex = Regex::new(r#"^##(.*)"#           ).unwrap();
-	static ref H1:     Regex = Regex::new(r#"^#(.*)"#            ).unwrap();
+	static ref H2:     Regex = Regex::new(r#"(^|\n)##(.*)"#      ).unwrap();
+	static ref H1:     Regex = Regex::new(r#"(^|\n)#(.*)"#       ).unwrap();
 	static ref BOLD:   Regex = Regex::new(r#"\*\*(.*?)\*\*"#     ).unwrap();
 	static ref ITALIC: Regex = Regex::new(r#"\*(.*?)\*"#         ).unwrap();
 	static ref STRIKE: Regex = Regex::new(r#"~~(.*?)~~"#         ).unwrap();
 }
 
 pub fn convert(s: &str) -> String {
-	let s = LINK   .replace(&s, "[url=$2]$1[/url]");
-	let s = CODE3  .replace(&s, "[code]$1[/code]");
-	let s = CODE2  .replace(&s, "[code]$1[/code]");
-	let s = CODE1  .replace(&s, "[code]$1[/code]");
-	let s = H2     .replace(&s, "[b]$1[/b]");
-	let s = H1     .replace(&s, "[h1]$1[/h1]");
-	let s = BOLD   .replace(&s, "[b]$1[/b]");
-	let s = ITALIC .replace(&s, "[i]$1[/i]");
-	let s = STRIKE .replace(&s, "[strike]$1[/strike]");
+	let s = LINK   .replace_all(&s, "[url=$2]$1[/url]"   );
+	let s = CODE3  .replace_all(&s, "[code]$1[/code]"    );
+	let s = CODE2  .replace_all(&s, "[code]$1[/code]"    );
+	let s = CODE1  .replace_all(&s, "[code]$1[/code]"    );
+	let s = H2     .replace_all(&s, "$1[b]$2[/b]"          );
+	let s = H1     .replace_all(&s, "$1[h1]$2[/h1]"        );
+	let s = BOLD   .replace_all(&s, "[b]$1[/b]"          );
+	let s = ITALIC .replace_all(&s, "[i]$1[/i]"          );
+	let s = STRIKE .replace_all(&s, "[strike]$1[/strike]");
 	String::from(s)
 }
 
@@ -33,6 +33,7 @@ fn test() {
 ```a```
 ``a``
 `a`
+###test
 ##hello
 #goodbye
 **test**
@@ -43,6 +44,7 @@ fn test() {
 [code]a[/code]
 [code]a[/code]
 [code]a[/code]
+[b]#test[/b]
 [b]hello[/b]
 [h1]goodbye[/h1]
 [b]test[/b]
