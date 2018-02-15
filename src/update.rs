900b0e5bf7666983adb96ea600bd3ccdafff0992
Las <las@protonmail.ch>
2018-02-15 17:10:59 +0100

    Link stdout of vbscript to stdout of laspad

--- src/update.rs
+++ src/update.rs
@@ -81,16 +81,18 @@ pub fn specific(dep: &str) -> Result<()> {
 		if cfg!(target_os = "windows") {
 			use std::process::Command;
 			use std::env::current_exe;
+			use std::io;
 
 			let path = dep_path.join(".mod.zip");
 			File::create(&path).unwrap().write_all(&buf).unwrap();
-			Command::new("cscript")
+			let _ = io::stdout().write(&Command::new("cscript")
 				.arg("//B")
 				.arg(current_exe().unwrap().join("unzip.vbs"))
 				.arg(&path)
 				.arg(dep_path)
 				.output()
-				.unwrap_or_else(|e| panic!("Could not read zip archive for {} @ {}: {:?}", dep, url, e));
+				.unwrap_or_else(|e| panic!("Could not read zip archive for {} @ {}: {:?}", dep, url, e))
+				.stdout);
 		} else {
 			let mut archive = ZipArchive::new(Cursor::new(buf)).unwrap_or_else(|e| panic!("Could not read zip archive for {} @ {}: {:?}", dep, url, e));
 			for i in 0..archive.len() {
