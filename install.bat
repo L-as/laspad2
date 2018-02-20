ed7ac67a1ef92392d81ab6a9a72dcbbff480a020
Las <las@protonmail.ch>
2018-02-20 10:53:31 +0100

    Fix installation script for windows

--- install.bat
+++ install.bat
@@ -7,7 +7,6 @@ cargo build --release
 copy "steam_api.dll"             "%HOMEPATH%\.cargo\bin\"
 copy "steam_appid.txt"           "%HOMEPATH%\.cargo\bin\"
 copy "target\release\laspad.exe" "%HOMEPATH%\.cargo\bin\"
-copy "src\unzip.vbs"             "%HOMEPATH%\.cargo\bin\"
 @echo laspad has been successfully installed into ~/.cargo/bin (also in your PATH)
 
 pause
