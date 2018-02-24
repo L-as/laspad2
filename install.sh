caeab513df0e8a881f348b6f4ac1758ac6b6e31e
Las <las@protonmail.ch>
2018-02-24 20:57:57 +0100

    Improve and fix linux installation script
    libsteam.api wasn't fetched from the correct path.
    Also now checks for LASPAD_DEBUG env var, which if true,
    makes the build a debug build

--- install.sh
+++ install.sh
@@ -4,12 +4,19 @@ echo -E '
 Set INSTALL_DIR to where laspad should be installed. Default is /usr/local.
 '
 
-set -e
-set -x
+cd "$(dirname "$0")"
+
+set -ex
 
 INSTALL_DIR="$(realpath -s "${INSTALL_DIR:=/usr/local}")"
 mkdir -p "$INSTALL_DIR/lib/laspad"
 
-cargo build --release
-cp target/release/laspad steam_appid.txt libsteam_api.so "$INSTALL_DIR/lib/laspad/"
+test "x$LASPAD_DEBUG" = xtrue && {
+	cargo build
+	cp target/debug/laspad steam_appid.txt 3rdparty/libsteam_api.so "$INSTALL_DIR/lib/laspad/"
+} || {
+	cargo build --release
+	cp target/release/laspad steam_appid.txt 3rdparty/libsteam_api.so "$INSTALL_DIR/lib/laspad/"
+}
+
 ln -sf "$INSTALL_DIR/lib/laspad/laspad" "$INSTALL_DIR/bin/laspad"
