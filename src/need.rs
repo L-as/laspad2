44828b8f08d75b97437356c8d6dc07dc47bada2c
Las <las@protonmail.ch>
2018-03-08 13:25:34 +0100

    Validate mod id for dependencies

--- src/need.rs
+++ src/need.rs
@@ -9,12 +9,16 @@ use common;
 pub enum NeedError {
 	#[fail(display = "Dependency already exists")]
 	AlreadyExists,
+	#[fail(display = "Mod ID is not valid")]
+	InvalidModId,
 }
 
 type Result = ::std::result::Result<(), Error>;
 
 pub fn main(dep: &str) -> Result {
 	common::find_project()?;
+
+	ensure!(u64::from_str_radix(dep, 16).is_ok(), NeedError::InvalidModId);
 
 	fs::create_dir_all("dependencies")?;
 
