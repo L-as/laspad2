6b3cc6151869d63c646310a36d49f7cfb01aaadd
Las <las@protonmail.ch>
2018-03-11 16:54:44 +0100

    Simplify the internals of the builder

--- src/builder.rs
+++ src/builder.rs
@@ -1,6 +1,5 @@
 use std::{
 	path::{self, Path, PathBuf},
-	collections::HashMap,
 	process::Command,
 	fs,
 };
@@ -9,29 +8,9 @@ use common::*;
 
 type Result<T> = ::std::result::Result<T, Error>;
 
-// dst is relative to the output directory
-type RuleHandler = fn(src: &Path, dst: &Path) -> Result<(Vec<PathBuf>, Command)>;
-
-lazy_static! {
-	static ref RULES: HashMap<&'static str, RuleHandler> = {
-		let mut map = HashMap::new();
-		let rules: &[(&'static str, RuleHandler)] = &[
-			("level", |src, dst| Ok((
-				{
-					let mut s = dst.to_str().unwrap().to_owned();
-					let sep   = path::MAIN_SEPARATOR;
-					let pos   = s.as_bytes().iter().rposition(|&c| c == sep as u8).map_or(0, |p| p+1);
-					s.insert_str(pos, "overviews/");
-					let overview = PathBuf::from(s);
-					vec![dst.to_owned(), overview.with_extension("tga"), overview.with_extension("hmp")]
-				}, cmd!((get_ns2().join("Overview.exe")) (src) compiled)
-			))),
-		];
-		for &(k, v) in rules.iter() {
-			map.insert(k, v);
-		};
-		map
-	};
+struct Rule {
+	outputs: Vec<PathBuf>,
+	cmd:     Command,
 }
 
 pub struct Builder {
@@ -42,13 +21,33 @@ pub struct Builder {
 }
 
 impl Builder {
+	fn get_rule(&mut self, src: &Path, dst: &Path) -> Option<Rule> {
+		if let Some(extension) = src.extension().and_then(|s| s.to_str()) {
+			match extension {
+				"level" => {
+					let mut s = dst.to_str().unwrap().to_owned();
+					let sep   = path::MAIN_SEPARATOR;
+					let pos   = s.as_bytes().iter().rposition(|&c| c == sep as u8).map_or(0, |p| p+1);
+					s.insert_str(pos, "overviews/");
+					let overview = PathBuf::from(s);
+					Some(Rule {
+						outputs: vec![dst.to_owned(), overview.with_extension("tga"), overview.with_extension("hmp")],
+						cmd:     cmd!((get_ns2().join("Overview.exe")) (src) compiled),
+					})
+				},
+				_ => None,
+			}
+		} else {
+			None
+		}
+	}
+
 	pub fn new(new: PathBuf, old: Option<PathBuf>) -> Self {
 		Builder {rest: Vec::new(), rest_built: false, new, old}
 	}
 
 	pub fn build(&mut self, src: &Path, dst: &Path) -> Result<()> {
-		if let Some(rule) = src.extension().and_then(|s| s.to_str()).and_then(|s| RULES.get(s)) {
-			let (outputs, mut cmd) = rule(src, dst)?;
+		if let Some(Rule {outputs, mut cmd}) = self.get_rule(src, dst) {
 			let old = self.old.as_ref();
 			if old.map_or(true, |old| !outputs.iter().all(|o| old.join(o).exists())) {
 				let status = cmd.status()?;
