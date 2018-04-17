3319ef8a7b5c88eaee64996aa6889cc8a7331973
Las <las@protonmail.ch>
2018-04-17 11:37:48 +0200

    Fix package.bat

--- package.bat
+++ package.bat
@@ -4,9 +4,12 @@ setlocal ENABLEEXTENSIONS
 
 cargo build --release
 
+del laspad.7z
 md laspad
 
 copy "3rdparty\steam_api64.dll"  laspad
 copy "steam_appid.txt"           laspad
 copy "laspad.bat"                laspad
-copy "target\release\laspad.exe" laspad
+copy "target\release\laspad.exe" laspad\laspad-gui.exe
+
+7z a laspad.7z laspad
